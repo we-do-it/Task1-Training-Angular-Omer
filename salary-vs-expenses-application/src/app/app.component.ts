8630d7002c5cd6c55a217b42e0eb4baf3fd91c8e
OmerCanOzdemirWeDoIT <omer.can.ozdemir@wedoit.be>
2022-10-13 09:46:14 +0200

    feat: validation and info

--- salary-vs-expenses-application/src/app/app.component.ts
+++ salary-vs-expenses-application/src/app/app.component.ts
@@ -8,16 +8,43 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title:string = 'salary-vs-expenses-application';
 
+  styleAlerts?:string;
+
   salary:number = 0;
 
   inputValue:number = 0;
 
+  checkValidation:boolean = true;
+
+  checkUpdateSalary: boolean = true;
+
+  isIncreased?:boolean;
+
+
   increase(){
-    this.salary += this.inputValue;
-    console.log(this.salary);
+    
+    if(this.checkValidationForm(this.inputValue)){
+      this.salary += this.inputValue;
+      this.checkValidation = true;
+      this.checkUpdateSalary = false;
+      this.isIncreased = true;
+      this.styleAlerts = 'flex place-content-center mt-5 bg-green-100  border border-green-400 text-green-700 px-4 py-3 rounded';
+    }else this.checkValidation = false;
+   
+    
   }
   decrease(){
-    this.salary -=this.inputValue;
+    if(this.checkValidationForm(this.inputValue)){
+      this.salary -=this.inputValue;
+      this.checkValidation = true;
+      this.checkUpdateSalary = false;
+      this.isIncreased = false;
+      this.styleAlerts = 'flex place-content-center mt-5 bg-red-100  border border-red-400 text-red-700 px-4 py-3 rounded';
+    }else this.checkValidation = false;
+  }
+
+  checkValidationForm(intputValue:number | null){
+    return intputValue === null ? false : true;
   }
 
 }
