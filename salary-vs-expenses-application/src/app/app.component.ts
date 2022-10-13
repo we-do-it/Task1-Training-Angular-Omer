590524b67e91696c756890b94e1317538dc6a36d
OmerCanOzdemirWeDoIT <omer.can.ozdemir@wedoit.be>
2022-10-13 10:01:43 +0200

    fix: bugfix and regroup functions to one function

--- salary-vs-expenses-application/src/app/app.component.ts
+++ salary-vs-expenses-application/src/app/app.component.ts
@@ -20,30 +20,28 @@ export class AppComponent {
 
   isIncreased?:boolean;
 
+  transactionValue?:number;
 
-  increase(){
-    
+  handleTransaction(isIncreased:boolean){
     if(this.checkValidationForm(this.inputValue)){
-      this.salary += this.inputValue;
+      this.transactionValue = this.inputValue;
       this.checkValidation = true;
       this.checkUpdateSalary = false;
-      this.isIncreased = true;
-      this.styleAlerts = 'flex place-content-center mt-5 bg-green-100  border border-green-400 text-green-700 px-4 py-3 rounded';
+      if(isIncreased){
+        this.salary += this.inputValue;
+        this.isIncreased = true;
+        this.styleAlerts = 'flex place-content-center mt-5 bg-green-100  border border-green-400 text-green-700 px-4 py-3 rounded';
+      }else{
+        this.salary -=this.inputValue;
+        this.isIncreased = false;
+        this.styleAlerts = 'flex place-content-center mt-5 bg-red-100  border border-red-400 text-red-700 px-4 py-3 rounded';
+      }
     }else this.checkValidation = false;
-   
-    
-  }
-  decrease(){
-    if(this.checkValidationForm(this.inputValue)){
-      this.salary -=this.inputValue;
-      this.checkValidation = true;
-      this.checkUpdateSalary = false;
-      this.isIncreased = false;
-      this.styleAlerts = 'flex place-content-center mt-5 bg-red-100  border border-red-400 text-red-700 px-4 py-3 rounded';
-    }else this.checkValidation = false;
+
   }
 
   checkValidationForm(intputValue:number | null){
+    this.checkUpdateSalary = true;
     return intputValue === null ? false : true;
   }
 
